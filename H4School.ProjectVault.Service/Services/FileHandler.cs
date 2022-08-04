129a84f8308cf1ff989cc7ddf5d94983eec4c895
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2022-08-04 18:49:06 +0200

    Added Certificate to try and see if I can Encrypt and decrypt with that - Encrypting is working now; still have to test Decryption.

--- H4School.ProjectVault.Service/Services/FileHandler.cs
+++ H4School.ProjectVault.Service/Services/FileHandler.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,27 +22,39 @@ namespace H4School.ProjectVault.Service.Services
 
         public static void EncryptFile(string fileName)
         {
-            SymmetricEncryption _aes = new();
-            EncryptedPacketDTO encryptedPacket = new EncryptedPacketDTO();
-            encryptedPacket.EncryptedSessionKey = _aes.GenerateRandomNumber(32);
-            encryptedPacket.Iv = _aes.GenerateRandomNumber(16);
+            #region Symmetric
+            //SymmetricEncryption _aes = new();
+            //EncryptedPacketDTO encryptedPacket = new EncryptedPacketDTO();
+            //encryptedPacket.EncryptedSessionKey = _aes.GenerateRandomNumber(32);
+            //encryptedPacket.Iv = _aes.GenerateRandomNumber(16);
+
+            // Choosing which filetext to read from by using our fileName variable.
+            //var fileText = File.ReadAllText(@"C:\Users\Jasmin\source\repos\H4School.ProjectVault\SecuredPasswords\" + fileName + ".txt");
+            // Encrypting the above variable using Symmetric encryption.
+            //string encrypted = Convert.ToBase64String(_aes.Encrypt(Encoding.UTF8.GetBytes(fileText), encryptedPacket.EncryptedSessionKey, encryptedPacket.Iv));
+            #endregion
+
+            X509Certificate2 myCertificate = Certificate.LoadCertificate(StoreLocation.CurrentUser, "CN=CryptoCert");
 
             // Choosing which filetext to read from by using our fileName variable.
             var fileText = File.ReadAllText(@"C:\Users\Jasmin\source\repos\H4School.ProjectVault\SecuredPasswords\" + fileName + ".txt");
-
-            // Encrypting the above variable using Symmetric encryption.
-            string encrypted = Convert.ToBase64String(_aes.Encrypt(Encoding.UTF8.GetBytes(fileText), encryptedPacket.EncryptedSessionKey, encryptedPacket.Iv));
-
-            SavingEncryptedFiles(encrypted, fileName);            
+            if (fileText != "")
+            {
+                string encrypted = Certificate.Encrypt(myCertificate, fileText);
+                SavingEncryptedFiles(encrypted, fileName);
+            }        
         }
 
         public static void DecryptData(string fileName)
         {
-            SymmetricEncryption _aes = new();
-            EncryptedPacketDTO encryptedPacketDTO = new EncryptedPacketDTO();  
+            X509Certificate2 myCertificate = Certificate.LoadCertificate(StoreLocation.CurrentUser, "CN=CryptoCert");
 
             var fileText = File.ReadAllText(@"C:\Users\Jasmin\source\repos\H4School.ProjectVault\SecuredPasswords\EncryptedPasswords\" + fileName + ".txt");
-            //string decrypted = Convert.ToBase64String(_aes.Decrypt(Encoding.UTF8.GetBytes(fileText), )
+            if (fileText != "")
+            {
+                string decrypted = Certificate.Decrypt(myCertificate, fileText);
+                SavingDecryptedFiles(decrypted, fileName);
+            }
         }
 
         public static void SavingEncryptedFiles(string encryptedData, string fileName)
@@ -50,6 +63,11 @@ namespace H4School.ProjectVault.Service.Services
             {
                 File.WriteAllText(@"C:\Users\Jasmin\source\repos\H4School.ProjectVault\SecuredPasswords\EncryptedPasswords\" + fileName + ".txt", encryptedData);
             }
+        }
+
+        public static void SavingDecryptedFiles(string decryptedData, string fileName)
+        {
+            File.WriteAllText(@"C:\Users\Jasmin\source\repos\H4School.ProjectVault\SecuredPasswords\DecryptedPasswords" + fileName + ".txt", decryptedData);
         }
     }
 }
